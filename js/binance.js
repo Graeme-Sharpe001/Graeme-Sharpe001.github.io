1bd814af65ac6eb4cb56ad8371d9325e868aea6e
Graeme Sharpe <gsharpe9@gatech.edu>
2019-04-29 19:24:29 -0400

    Issue #005: Update to Binance Trader
    bug fix

--- js/binance.js
+++ js/binance.js
@@ -17,8 +17,27 @@
 //     }
 //     request.send();
 // }
+firebase.auth().onAuthStateChanged(function(userSignedIn) {
+    if (userSignedIn) {
+        // User is signed in.
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+
+        var user = firebase.auth().currentUser;
+
+        if (user != null) {
+            var email = user.email;
+        }
+    } else {
+        // No user is signed in.
+        document.getElementById("user_div").style.display = "none";
+        document.getElementById("login_div").style.display = "block";
+        window.alert("not signed in");
+    }
+});
 
 function wsTest() {
+    init();
     const wsAggTrade = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@aggTrade', undefined);
 
     wsAggTrade.onmessage = function incoming(event) {
@@ -27,15 +46,6 @@ function wsTest() {
         var msg = JSON.parse(event.data);
         var currentPrice = msg.p;
         document.getElementById("currentPrice").innerHTML = currentPrice;
-        // var li = document.createElement("li");
-        // li.appendChild(document.createTextNode(currentPrice));
-        // document.getElementById("orderBook").appendChild(li);
-
-        // switch (msg.type) {
-        //     case "p":
-        //         text = "Price: " + msg.p;
-        //         console.log(text);
-        // }
     };
 
     const wsDepth = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@depth10', undefined);
@@ -67,19 +77,76 @@ function wsTest() {
             document.getElementById("bids").removeChild(liBids[i]);
 
         }
-        //
-        // for(i = 0, il = liAsks.length;i<il;i++) {
-        //     document.getElementById("asks").removeChild(liAsks[i]);
-        //     document.getElementById("bids").removeChild(liBids[i]);
-        // }
-
-        // document.getElementById("currentPrice").innerHTML = currentPrice;
-
-
-        // switch (msg.type) {
-        //     case "p":
-        //         text = "Price: " + msg.p;
-        //         console.log(text);
-        // }
     };
+
+    function init() {
+        var db = firebase.firestore();
+
+        var user = firebase.auth().currentUser;
+        window.alert(user.email);
+
+        var docRef = db.collection("users").doc(user.email);
+
+        docRef.get().then(function(doc) {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                console.log("Username: ", doc.data().username);
+                document.getElementById("name").innerText = "Username: " + doc.get("username");
+                document.getElementById("initDollars").innerText = "Initial Dollars: " + doc.get("initUSD");
+                document.getElementById("portValue").innerText = "Current Portfolio Value: " + doc.get("portval");
+                document.getElementById("usd").innerText = "USD: " + doc.get("usd");
+                document.getElementById("btc").innerText = "BTC: " + doc.get("btc");
+                document.getElementById("profit").innerText = "Profit: " + doc.get("profit");
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        })
+    }
 }
+
+
+
+
+    function buy() {
+        let amount = document.getElementById(buy).innerHTML;
+        let price = document.getElementById("currentPrice").innerHTML;
+        window.alert(amount + " " + price);
+
+    }
+
+    function sell() {
+        // // Initialize Cloud Firestore through Firebase
+        // firebase.initializeApp({
+        //     apiKey: "AIzaSyBU8qTyI8KRLzFt6X-lQQDIQKPefeFZiP4",
+        //     authDomain: "graeme-sharpe-website.firebaseapp.com",
+        //     projectId: 'graeme-sharpe-website'
+        // });
+
+        var db = firebase.firestore();
+
+        var user = firebase.auth().currentUser;
+        window.alert(user.email);
+
+        var docRef = db.collection("users").doc(user.email);
+
+        docRef.get().then(function(doc) {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+
+        // db.collection("users").get().then((querySnapshot) => {
+        //     querySnapshot.forEach((doc) => {
+        //         console.log(`${doc.id} => ${doc.data()}`);
+        //     });
+        // });
+
+    }

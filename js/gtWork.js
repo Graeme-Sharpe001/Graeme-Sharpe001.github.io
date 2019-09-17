b9fff1ee60930b213ec7493fb07d2080a3e147d4
Graeme Sharpe <gsharpe9@gatech.edu>
2019-09-16 21:21:00 -0400

    Issue #006: Add/Clean Up Content
    Deployed multiple bug fixes and UI clean ups. Added new Git Gists and expanding content. Updated Resume.

---
+++ js/gtWork.js
@@ -0,0 +1,35 @@
+function expand_1() {
+
+    var x = document.getElementById("expand_1");
+
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+}
+
+function expand_2() {
+
+    var x = document.getElementById("expand_2");
+
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+}
+
+function expand_3() {
+
+    var x = document.getElementById("expand_3");
+
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+}

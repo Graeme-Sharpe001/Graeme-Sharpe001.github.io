ea18f5c0ae0241e088db751c62dfcce96d56f524
Graeme Sharpe <gsharpe9@gatech.edu>
2019-09-26 09:07:49 -0400

    Content Update
    Cleaning and pushing content to gt-work

--- js/gtWork.js
+++ js/gtWork.js
@@ -1,30 +1,6 @@
-function expand_1() {
+function expands(element) {
 
-    var x = document.getElementById("expand_1");
-
-    if (x.style.display == "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-
-}
-
-function expand_2() {
-
-    var x = document.getElementById("expand_2");
-
-    if (x.style.display == "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-
-}
-
-function expand_3() {
-
-    var x = document.getElementById("expand_3");
+    var x = document.getElementById(element);
 
     if (x.style.display == "block") {
         x.style.display = "none";
